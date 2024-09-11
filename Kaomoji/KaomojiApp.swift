f7758060b021544c5f7751ac9ab1d67cd57e36a4
jinz32 <62317006+jinz32@users.noreply.github.com>
2024-09-11 11:37:09 -0700

    Initial Commit

---
+++ Kaomoji/KaomojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaomojiApp.swift
+//  Kaomoji
+//
+//  Created by Jonathan Zheng on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KaomojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
